6c5c95f251b7179ac72d36cbaf9f5f25c25e9bf0
Ashley <ashleyuy@hotmail.com>
2014-06-10 23:45:37 +0800

    ex3.py

---
+++ ex3.py
@@ -0,0 +1,30 @@
+#This is me typing what I'm doing
+print "I am using mathematical operations like a boss:"
+print "My Swag", 25+30/6
+print "My Awesomeness", 100-25*3%4
+
+print "Now I will count my Narcissism"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Will my sister calculate 3+2<5-7?"
+print 3+2<5-7
+
+print " What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+#She must be watching me now
+print "I think this program is lying about my sister."
+
+print "I want to try drugs but I know I shouldn't."
+#Please say true
+print "Pancakes are evil", 5>-2
+
+print "Bo's in Bohol need better coffe", 5>=-2
+#I'm going to sleep anyway
+print "It's late, should I sleep?", 5<=-2
+
+print "Let's test floating points to get extra credit", 0.1+0.2
+
+print round(2.675,2)
+
+print 1/10+2/100+5/1000
