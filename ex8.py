7fe09f37be310a794fcd60979b2bca4de22ddcd4
Ashley <ashleyuy@hotmail.com>
2014-06-11 11:15:05 +0800

    Changes some text in ex8.py

--- ex8.py
+++ ex8.py
@@ -6,7 +6,7 @@ print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, False, True)
 print formatter % (formatter, formatter, formatter, formatter)
 #they might be new line-ed here but they're actually connected because of the commas
-# Double quotes here come out as single quotes when printed
+# Double quotes here come out as single quotes when printed because %r print as is
 print formatter % (
     "I had this thing.",
     "That you could type up right.",
