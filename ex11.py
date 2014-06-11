37662c078d357e5b1fc7bd22c34606d4e1016af2
Ashley <ashleyuy@hotmail.com>
2014-06-11 11:49:51 +0800

    Started on exercise11

---
+++ ex11.py
@@ -0,0 +1,21 @@
+#omg there are 52 exercises and I'm still at 11 D: 
+# I have to do 26 today to get to half!
+#the comma is so that the ans is still in the same line as the ques
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+print "Extra points: \n"
+
+print "Number 1:", 
+num1 = int(raw_input)
+print "Number 2:",
+num2 = int(raw_input)
+print "The sum is", num1 + num2
+
