4f11939ffb0f4ff7261602a2b9257c704e213a7f
Ashley <ashleyuy@hotmail.com>
2014-06-11 11:18:07 +0800

    Did exercise 9! How many are there?

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#wow newline exists in python
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+#this is like the <pre> tag in html
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
