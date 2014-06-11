40fdb51c49ed54cbdaf7079342c74666003cf166
Ashley <ashleyuy@hotmail.com>
2014-06-11 11:26:44 +0800

    Done with 10

---
+++ ex10.py
@@ -0,0 +1,18 @@
+#this is a tab! 
+tabby_cat = "\tI'm tabbed in."
+#one slash is like \n
+persian_cat = "I'm split\non a line."
+#double jus tmakes one print 
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
