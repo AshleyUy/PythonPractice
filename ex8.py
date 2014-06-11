ae8a2356203e62c49f564944401c4e6883428838
Ashley <ashleyuy@hotmail.com>
2014-06-11 11:13:30 +0800

    Done with exercise 8!

---
+++ ex8.py
@@ -0,0 +1,15 @@
+#okay, this is convenient
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+#they might be new line-ed here but they're actually connected because of the commas
+# Double quotes here come out as single quotes when printed
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
