191a167e267352d51088843df5025629cf93f5cc
Ashley <ashleyuy@hotmail.com>
2014-06-12 08:31:17 +0800

    Did exercise 19

---
+++ ex19.py
@@ -0,0 +1,35 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print "You have %d cheeses!" % cheese_count
+    print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "Man that's enough for a party!"
+    print "Get a blanket.\n"
+
+def add (num1, num2):
+    sum = num1 + num2
+    return sum
+
+print "Lets try adding numbers:"
+print "Num1: ",
+num1 = int(raw_input())
+print "Num2: ",
+num2 = int(raw_input())
+print "The answer is:"
+print add(num1, num2)
+
+print "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+
+print "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+print "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+
+print "And we can combine the two, variables and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
